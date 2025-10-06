ed35ec781f1f59b32e6220fe9d84dddee4597d9e
Divyesh Parikh <dcparikh360@gmail.com>
2025-10-06 10:27:52 +0900

    feat: added request context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -26,7 +26,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
-	protected := dynamic.Append(app.requireAuthentication)
+	protected := dynamic.Append(app.authenticate, app.requireAuthentication)
 
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
